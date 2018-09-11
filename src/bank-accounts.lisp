6d075fe3753d7d4775d231abad5a45a18971e4c9
Ram Vedam <rvedam@gmail.com>
2018-09-11 00:28:16 -0400

    Moved savings account class object into own file

--- src/bank-accounts.lisp
+++ src/bank-accounts.lisp
@@ -6,13 +6,6 @@
    (%type :reader account-type))
   (:documentation "Base class for all bank accounts"))
 
-(defclass savings-account (bank-account)
-  ((%overdraft-p :initform nil :reader account-overdraft-p)
-   (%savings-interest-rate :initform .05)))
-
-(afp-utils:define-printer (savings-account stream)
-  (format stream "~A" (account-type savings-account)))
-
 ;; API
 (defgeneric deposit (account amount)
   (:documentation "deposits money into account."))
@@ -20,19 +13,8 @@
 (defgeneric withdraw (account amount)
   (:documentation "withdraws money from account"))
 
-(defun open-savings-account (customer-name balance)
-  (make-instance 'savings-account :customer-name customer-name
-                 :balance balance))
-
-(defun update-savings-account-type (account)
-  (with-slots (%type) account
-    (with-accessors ((balance account-balance)) account
-      (cond ((<= balance 1000) (setf %type :basic))
-            ((> balance 1000) (setf %type :premium))))))
-
-(defmethod initialize-instance :after ((account savings-account) &rest initargs)
-  (declare (ignore initargs))
-  (update-savings-account-type account))
+(defgeneric some-func (bank-account)
+  (:method-combination progn :most-specific-last))
 
 (defmethod deposit ((account bank-account) amount)
   (with-accessors ((balance account-balance)) account
@@ -42,3 +24,5 @@
   (with-accessors ((balance account-balance)) account
     (setf balance (- balance amount))))
 
+(defmethod some-func progn ((account bank-account))
+  (print "base class some-func called"))
